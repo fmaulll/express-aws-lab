158988338295a76ec24dc276636cd9c0d75f1842
fmaulll <90718125+fmaulll@users.noreply.github.com>
2026-06-11 23:02:03 +0700

    test deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS EC2!");
+    res.send("Version 2 - CI/CD Works!");
 });
 
 app.listen(3000, () => {
